5b2ac1ec62cebdfe156ace7cc05df1cb08dd3064
Youngwook-Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-10-12 18:21:11 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,17 @@
+// Created by: Youngwook
+// Created on: OCT 2022
+//
+// This program does basic math System;
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("The dimensions of a rectangle are: 5cm and 3cm");
+        Console.WriteLine("The area is : " + (5 * 3) + "㎠");
+        Console.WriteLine("The perimeter is : " + (2 * (5 + 3)) + "cm");
+        Console.WriteLine("\ndone.");
+    }
+}
